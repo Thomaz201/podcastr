6da23aca5b41ac078ec55ef8459e47cea7ae5b72
Lucas Thomaz <lucasthomazr@hotmail.com>
2021-04-23 02:21:09 -0300

    Json-server configured

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,22 @@
-export default function Home() {
+import axios from 'axios';
+
+export default function Home(props) {
   return (
-    <h1>Hello World</h1>  
+    <div>
+      <h1>Hello World</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>  
   )
+}
+
+export async function getStaticProps() {
+  const { data } = await axios.get('http://localhost:3333/episodes');
+  console.log('response', data)
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
 }
